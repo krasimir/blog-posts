57085e68d9f66936ce28f5869c79ac965bdaf512
Krasimir Tsonev <krasimir@outset.ws>
2012-10-15 09:44:03 +0300

    Updating the url of the site for testing

--- TestingWithZombieJS/nodejs/zombie.spec.js
+++ TestingWithZombieJS/nodejs/zombie.spec.js
@@ -1,5 +1,5 @@
 var Browser = require("zombie");
-var url = "http://blogposts.dev/TestingWithZombieJS/site/";
+var url = "http://work.krasimirtsonev.com/git/blog-posts/TestingWithZombieJS/site/";
 var browser = new Browser();
 
 describe("testing with zombie", function() {
